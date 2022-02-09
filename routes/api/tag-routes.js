732c917a8ee46af5bb54068b2eefc4158b58a977
DeathByDusk <elijahkanellakis@gmail.com>
2022-02-08 21:37:30 -0600

    cloned starter code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -76,7 +76,7 @@ router.put('/:id', (req, res) => {
   )
     .then(dbTagData => {
       if(!dbTagData) {
-        res.status(404).json({ message: 'No category found with this id!' });
+        res.status(404).json({ message: 'No tag found with this id!' });
         return;
       }
       res.json(dbTagData);
